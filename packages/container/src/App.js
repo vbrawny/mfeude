0af7d22607c6245237f62bc46eae238da8d5fd40
vbrawny <brawny.venky@gmail.com>
2022-02-25 09:23:52 +0000

    added header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 //import {mount} from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 //console.log(mount);//reference to html element and displays it
 //mount is not a react component
 
 export default () => {
     return (
-        <div>
-            <h1>Hi deploy new from Container!!!</h1>
-            <hr/>
-            <MarketingApp/>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     )
 }
