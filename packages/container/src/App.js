268554f6918b40988381b2a12a5f2eef4d8ffb3d
vbrawny <brawny.venky@gmail.com>
2022-02-25 10:16:44 +0000

    production classname collision fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,17 +3,23 @@ import { BrowserRouter } from 'react-router-dom';
 //import {mount} from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider,createGenerateClassName } from '@material-ui/core';
 
 //console.log(mount);//reference to html element and displays it
 //mount is not a react component
-
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 export default () => {
     return (
+        
         <BrowserRouter>
-            <div>
-                <Header/>
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header/>
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
